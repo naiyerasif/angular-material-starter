a8c66f3385b869b497bb60e35b85a42400a42098
Naiyer Asif <naiyer.cs@gmail.com>
2018-09-16 23:34:06 +0530

    Edit project metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Material Starter';
+  tagline = 'A simple starter project for Angular Material 6';
+  features = [
+      'Angular Material 6',
+      'Polyfills for IE',
+      'Native Font Stack (instead of Roboto)',
+      'Material Design Icons',
+      'SCSS for theme customization',
+      'Dependency Module to load Material Components'
+  ]
 }
